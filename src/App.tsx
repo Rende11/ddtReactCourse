1c96303d34990e3a65a1e3398f985f8071ba3b3b
Rende11 <artamonovn@gmail.com>
2022-01-10 19:38:11 +0300

    Move content outside the component

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Clock from './components/Clock'
 import ToggleButton  from './components/ToggleButton'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const content = <p>Made by <a href="https://github.com/Rende11/ddtReactCourse">@rende11.</a></p>
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Vite + React!</p>
         <Clock/>
-        <ToggleButton/>
+        <ToggleButton content = {content} />
       </header>
     </div>
   )
--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 
-export default function ToggleButton() {
+interface Props {
+    content: any
+}
+
+export default function ToggleButton(props: Props) {
     const [show, setToggleState] = useState(false);
 
     return (
         <div>
           <button
            type="button"
-           onClick={() => setToggleState((curr) => !curr) }>
+           onClick={() => setToggleState((curr) => !curr)}>
             Show info
           </button>
-          {show ? <p>Made by <a href="https://github.com/Rende11/ddtReactCourse">@rende11.</a></p> : null}
+          {show ? props.content : null}
         </div>
     );
 }
